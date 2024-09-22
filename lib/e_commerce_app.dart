45df59d3d6aec1394fb41cae76941e43e5ffe9f9
Amarkumar Belkhede <belkhedeamar@gmail.com>
2024-09-22 09:33:03 +0530

    Update e_commerce_app.dart

--- lib/e_commerce_app.dart
+++ lib/e_commerce_app.dart
@@ -1,2 +1,22 @@
 library e_commerce_app;
 
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ECommerceApp extends StatefulWidget {
+  const ECommerceApp({super.key});
+
+  @override
+  State<ECommerceApp> createState() => _ECommerceAppState();
+}
+
+class _ECommerceAppState extends State<ECommerceApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("E commerce app"),
+      ),
+    );
+  }
+}
